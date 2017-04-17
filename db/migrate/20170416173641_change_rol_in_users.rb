8fd56974afc2f65c333457a0884259102b9d2300
azmat-develpor <azmat.rana01@gmail.com>
2017-04-18 00:06:06 +0500

    sadfsad

--- db/migrate/20170416173641_change_rol_in_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeRolInUsers < ActiveRecord::Migration[5.0]
-  def change
-  	change_column :users, :role, :string, default: "user" 
-  end
-end
--- db/migrate/20170416174803_change_role_in_users.rb
+++
@@ -1,6 +0,0 @@
-class ChangeRoleInUsers < ActiveRecord::Migration[5.0]
-  def change
-  	change_column :users, :role, :string, default: "patient" 
-
-  end
-end
