9e7464d30f4b52005a037167ce8ca0d8f5d79920
azmat-develpor <azmat.rana01@gmail.com>
2017-05-12 01:05:12 +0500

    dfaf

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only:[:create, :update, :edit, :destory]
+  before_action :authenticate_user!, only:[:new,:create, :update, :edit, :destory]
   # GET /posts
   # GET /posts.json
   def index
